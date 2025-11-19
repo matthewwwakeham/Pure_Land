8a47d7eb302eec12f3be03752c16188adb4f75eb
Matthew Wakeham <mwakeham95@gmail.com>
2025-11-19 13:13:19 -0600

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_class=None):
+    app = Flask(
+        __name__,
+    )
+    
+    if config_class:
+        app.config.from_object(config_class)
+    else: 
+        from .config import Config
+        app.config.from_object(Config)
+        
+    db.init_app(app)
+    
+    from.routes import main
+    app.register_blueprint(main)
+    
+    @app.route("/helloworld")
+    def ping():
+        return {"status": "ok"}
+    
+    
+    return app
